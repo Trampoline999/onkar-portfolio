d56d0a10cad70d406ca73227f423675432aed9d6
Onkar Chougule <onkarchougule99@gmail.com>
2026-05-20 00:35:36 +0530

    feat: add new sticker asset icons and update RetroIcons component

--- components/RetroIcons.jsx
+++ components/RetroIcons.jsx
@@ -1,177 +1,21 @@
- import React from 'react';
+import React from 'react';
 import { motion } from "motion/react";
-
-const FolderIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-full h-full" fill="currentColor">
-    <g>
-      <path d="m30.472 3.045 -1.52 0 0 -1.52 -1.52 0 0 -1.53 -21.34 0 0 12.2 -4.57 0 0 1.52 7.62 0 0 -1.52 -1.52 0 0 -10.67 16.76 0 0 6.09 6.09 0 0 15.24 -1.52 0 0 3.05 1.52 0 0 4.57 1.53 0 0 -25.9 -1.53 0 0 -1.53z" strokeWidth={1} />
-      <path d="M7.622 30.475h22.85v1.52H7.622Z" strokeWidth={1} />
-      <path d="M27.432 19.805h1.52v3.05h-1.52Z" strokeWidth={1} />
-      <path d="M25.902 16.765h1.53v3.04h-1.53Z" strokeWidth={1} />
-      <path d="M10.672 15.235h15.23v1.53h-15.23Z" strokeWidth={1} />
-      <path d="M19.812 7.615h1.52v1.53h-1.52Z" strokeWidth={1} />
-      <path d="M15.242 10.665h4.57v1.53h-4.57Z" strokeWidth={1} />
-      <path d="M13.712 7.615h1.53v1.53h-1.53Z" strokeWidth={1} />
-      <path d="M9.142 13.715h1.53v1.52h-1.53Z" strokeWidth={1} />
-      <path d="M6.092 27.425h1.53v3.05h-1.53Z" strokeWidth={1} />
-      <path d="M4.572 24.385h1.52v3.04h-1.52Z" strokeWidth={1} />
-      <path d="M3.052 21.335h1.52v3.05h-1.52Z" strokeWidth={1} />
-      <path d="M1.522 18.285h1.53v3.05h-1.53Z" strokeWidth={1} />
-      <path d="M0.002 13.715h1.52v4.57H0.002Z" strokeWidth={1} />
-    </g>
-  </svg>
-);
-
-const CdIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-full h-full" fill="currentColor">
-    <g>
-      <path d="m30.48 9.14 -1.53 0 0 4.58 -6.09 0 0 1.52 1.52 0 0 1.52 1.53 0 0 -1.52 1.52 0 0 1.52 1.52 0 0 -1.52 1.53 0 0 1.52 -1.53 0 0 1.53 1.53 0 0 1.52 1.52 0 0 -7.62 -1.52 0 0 -3.05z" strokeWidth={1} />
-      <path d="m28.95 21.33 -1.52 0 0 -1.52 -1.52 0 0 -1.52 -1.53 0 0 1.52 -1.52 0 0 -1.52 -1.53 0 0 -1.53 1.53 0 0 -1.52 -1.53 0 0 -1.52 -1.52 0 0 4.57 -1.52 0 0 1.52 -4.58 0 0 1.52 4.58 0 0 1.53 1.52 0 0 3.05 1.52 0 0 3.04 -1.52 0 0 1.53 3.05 0 0 -1.53 3.05 0 0 -1.52 1.52 0 0 -1.52 1.52 0 0 -3.05 1.53 0 0 -3.05 -1.53 0 0 1.52z" strokeWidth={1} />
-      <path d="M27.43 18.29h1.52v1.52h-1.52Z" strokeWidth={1} />
-      <path d="M27.43 6.1h1.52v3.04h-1.52Z" strokeWidth={1} />
-      <path d="M25.91 16.76h1.52v1.53h-1.52Z" strokeWidth={1} />
-      <path d="M25.91 4.57h1.52V6.1h-1.52Z" strokeWidth={1} />
-      <path d="M22.86 16.76h1.52v1.53h-1.52Z" strokeWidth={1} />
-      <path d="M22.86 3.05h3.05v1.52h-3.05Z" strokeWidth={1} />
-      <path d="M19.81 1.53h3.05v1.52h-3.05Z" strokeWidth={1} />
-      <path d="M18.29 12.19h1.52v1.53h-1.52Z" strokeWidth={1} />
-      <path d="M12.19 30.48h7.62V32h-7.62Z" strokeWidth={1} />
-      <path d="M18.29 13.72h-4.58v4.57h4.58Zm-1.53 3.04h-1.52v-1.52h1.52Z" strokeWidth={1} />
-      <path d="m13.71 6.1 1.53 0 0 -4.57 4.57 0 0 -1.53 -7.62 0 0 1.53 1.52 0 0 4.57z" strokeWidth={1} />
-      <path d="M12.19 18.29h1.52v1.52h-1.52Z" strokeWidth={1} />
-      <path d="M9.14 28.95h3.05v1.53H9.14Z" strokeWidth={1} />
-      <path d="m12.19 13.72 1.52 0 0 -1.53 4.58 0 0 -1.52 -1.53 0 0 -4.57 -1.52 0 0 4.57 -1.53 0 0 -1.53 -1.52 0 0 3.05 -3.05 0 0 1.53 1.53 0 0 4.57 1.52 0 0 -4.57z" strokeWidth={1} />
-      <path d="M10.67 6.1h1.52v3.04h-1.52Z" strokeWidth={1} />
-      <path d="M9.14 24.38h3.05v1.53H9.14Z" strokeWidth={1} />
-      <path d="M6.1 27.43h3.04v1.52H6.1Z" strokeWidth={1} />
-      <path d="M7.62 22.86h1.52v1.52H7.62Z" strokeWidth={1} />
-      <path d="M6.1 10.67h3.04v1.52H6.1Z" strokeWidth={1} />
-      <path d="m9.14 6.1 1.53 0 0 -3.05 1.52 0 0 -1.52 -3.05 0 0 1.52 -3.04 0 0 1.52 3.04 0 0 1.53z" strokeWidth={1} />
-      <path d="M6.1 19.81h1.52v3.05H6.1Z" strokeWidth={1} />
-      <path d="M4.57 25.91H6.1v1.52H4.57Z" strokeWidth={1} />
-      <path d="M4.57 4.57H6.1V6.1H4.57Z" strokeWidth={1} />
-      <path d="M3.05 22.86h1.52v3.05H3.05Z" strokeWidth={1} />
-      <path d="M1.52 19.81h1.53v3.05H1.52Z" strokeWidth={1} />
-      <path d="m3.05 10.67 3.05 0 0 -1.53 -1.53 0 0 -3.04 -1.52 0 0 3.04 -1.53 0 0 3.05 1.53 0 0 -1.52z" strokeWidth={1} />
-      <path d="M0 12.19h1.52v7.62H0Z" strokeWidth={1} />
-    </g>
-  </svg>
-);
-
-const CassetteIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-full h-full" fill="currentColor">
-    <g>
-      <path d="M30.48 6.095H32v19.81h-1.52Z" strokeWidth={1} />
-      <path d="m25.91 25.905 0 -1.52 -1.53 0 0 1.52 -16.76 0 0 -1.52 -1.52 0 0 1.52 -4.57 0 0 1.52 28.95 0 0 -1.52 -4.57 0z" strokeWidth={1} />
-      <path d="M27.43 22.855h1.53v1.53h-1.53Z" strokeWidth={1} />
-      <path d="M27.43 7.615h1.53v1.53h-1.53Z" strokeWidth={1} />
-      <path d="M27.43 13.715h-1.52V12.2H6.1v1.52H4.58v3.05H6.1v1.52h19.81v-1.52h1.52Zm-16.76 3.05H7.62v-3.05h3.05Zm7.62 -1.53h-4.57v-1.52h4.57Zm6.09 1.53h-3.04v-3.05h3.04Z" strokeWidth={1} />
-      <path d="M22.86 22.855h1.52v1.53h-1.52Z" strokeWidth={1} />
-      <path d="M9.15 21.335h13.71v1.52H9.15Z" strokeWidth={1} />
-      <path d="M9.15 7.615h13.71v1.53H9.15Z" strokeWidth={1} />
-      <path d="M7.62 22.855h1.53v1.53H7.62Z" strokeWidth={1} />
-      <path d="M3.05 22.855h1.53v1.53H3.05Z" strokeWidth={1} />
-      <path d="M3.05 7.615h1.53v1.53H3.05Z" strokeWidth={1} />
-      <path d="M1.53 4.575h28.95v1.52H1.53Z" strokeWidth={1} />
-      <path d="M0 6.095h1.53v19.81H0Z" strokeWidth={1} />
-    </g>
-  </svg>
-);
-
-const InternetIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-full h-full" fill="currentColor">
-    <g>
-      <path d="M30.47 9.14h-1.52v1.53H25.9V9.14h-1.52v1.53H7.62V9.14H6.09v1.53H3.05V9.14H1.52v3.05H0v7.62h1.52v3.05h1.53v-1.53h3.04v1.53h1.53v-1.53h16.76v1.53h1.52v-1.53h3.05v1.53h1.52v-3.05H32v-7.62h-1.53Zm-19.81 9.14H9.14v1.53H7.62v-1.53H6.09v1.53H4.57v-1.53H3.05v-4.57h1.52v4.57h1.52v-3.04h1.53v3.04h1.52v-4.57h1.52Zm9.15 0h-1.53v1.53h-1.52v-1.53h-1.52v1.53h-1.53v-1.53h-1.52v-4.57h1.52v4.57h1.53v-3.04h1.52v3.04h1.52v-4.57h1.53Zm9.14 0h-1.52v1.53H25.9v-1.53h-1.52v1.53h-1.53v-1.53h-1.52v-4.57h1.52v4.57h1.53v-3.04h1.52v3.04h1.53v-4.57h1.52Z" strokeWidth={1} />
-      <path d="M27.43 22.86h1.52v3.04h-1.52Z" strokeWidth={1} />
-      <path d="M27.43 6.09h1.52v3.05h-1.52Z" strokeWidth={1} />
-      <path d="M25.9 25.9h1.53v1.53H25.9Z" strokeWidth={1} />
-      <path d="M25.9 4.57h1.53v1.52H25.9Z" strokeWidth={1} />
-      <path d="m25.9 7.62 0 -1.53 -4.57 0 0 -1.52 -1.52 0 0 3.05 3.04 0 0 1.52 1.53 0 0 -1.52 1.52 0z" strokeWidth={1} />
-      <path d="M22.85 27.43h3.05v1.52h-3.05Z" strokeWidth={1} />
-      <path d="m22.85 24.38 -3.04 0 0 3.05 1.52 0 0 -1.53 4.57 0 0 -1.52 -1.52 0 0 -1.52 -1.53 0 0 1.52z" strokeWidth={1} />
-      <path d="M22.85 3.05h3.05v1.52h-3.05Z" strokeWidth={1} />
-      <path d="M19.81 28.95h3.04v1.53h-3.04Z" strokeWidth={1} />
-      <path d="M19.81 1.52h3.04v1.53h-3.04Z" strokeWidth={1} />
-      <path d="M18.28 27.43h1.53v1.52h-1.53Z" strokeWidth={1} />
-      <path d="M18.28 3.05h1.53v1.52h-1.53Z" strokeWidth={1} />
-      <path d="m18.28 28.95 -1.52 0 0 1.53 -1.52 0 0 -1.53 -1.53 0 0 1.53 -1.52 0 0 1.52 7.62 0 0 -1.52 -1.53 0 0 -1.53z" strokeWidth={1} />
-      <path d="M12.19 7.62h7.62v1.52h-7.62Z" strokeWidth={1} />
-      <path d="M12.19 22.86h7.62v1.52h-7.62Z" strokeWidth={1} />
-      <path d="m13.71 3.05 1.53 0 0 -1.53 1.52 0 0 1.53 1.52 0 0 -1.53 1.53 0 0 -1.52 -7.62 0 0 1.52 1.52 0 0 1.53z" strokeWidth={1} />
-      <path d="M12.19 27.43h1.52v1.52h-1.52Z" strokeWidth={1} />
-      <path d="M12.19 3.05h1.52v1.52h-1.52Z" strokeWidth={1} />
-      <path d="M9.14 28.95h3.05v1.53H9.14Z" strokeWidth={1} />
-      <path d="M9.14 1.52h3.05v1.53H9.14Z" strokeWidth={1} />
-      <path d="M6.09 27.43h3.05v1.52H6.09Z" strokeWidth={1} />
-      <path d="m9.14 7.62 3.05 0 0 -3.05 -1.53 0 0 1.52 -4.57 0 0 1.53 1.53 0 0 1.52 1.52 0 0 -1.52z" strokeWidth={1} />
-      <path d="M6.09 3.05h3.05v1.52H6.09Z" strokeWidth={1} />
-      <path d="m6.09 24.38 0 1.52 4.57 0 0 1.53 1.53 0 0 -3.05 -3.05 0 0 -1.52 -1.52 0 0 1.52 -1.53 0z" strokeWidth={1} />
-      <path d="M4.57 25.9h1.52v1.53H4.57Z" strokeWidth={1} />
-      <path d="M4.57 4.57h1.52v1.52H4.57Z" strokeWidth={1} />
-      <path d="M3.05 22.86h1.52v3.04H3.05Z" strokeWidth={1} />
-      <path d="M3.05 6.09h1.52v3.05H3.05Z" strokeWidth={1} />
-    </g>
-  </svg>
-);
-
-const BrushIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-full h-full" fill="currentColor">
-    <g>
-      <path d="M30.48 1.52H32v4.57h-1.52Z" strokeWidth="1"></path>
-      <path d="M28.95 6.09h1.53v3.05h-1.53Z" strokeWidth="1"></path>
-      <path d="M27.43 0h3.05v1.52h-3.05Z" strokeWidth="1"></path>
-      <path d="M27.43 9.14h1.52v3.05h-1.52Z" strokeWidth="1"></path>
-      <path d="M25.9 12.19h1.53v3.05H25.9Z" strokeWidth="1"></path>
-      <path d="M25.9 1.52h1.53v3.05H25.9Z" strokeWidth="1"></path>
-      <path d="M24.38 15.24h1.52v3.04h-1.52Z" strokeWidth="1"></path>
-      <path d="M24.38 4.57h1.52v3.05h-1.52Z" strokeWidth="1"></path>
-      <path d="M22.86 22.86h1.52v4.57h-1.52Z" strokeWidth="1"></path>
-      <path d="M22.86 18.28h1.52v3.05h-1.52Z" strokeWidth="1"></path>
-      <path d="M22.86 7.62h1.52v3.05h-1.52Z" strokeWidth="1"></path>
-      <path d="M21.33 10.67h1.53v3.04h-1.53Z" strokeWidth="1"></path>
-      <path d="m21.33 25.9 -1.52 0 0 1.53 -1.52 0 0 -1.53 -1.53 0 0 -1.52 -1.52 0 0 1.52 -1.53 0 0 -3.04 -1.52 0 0 4.57 -1.52 0 0 1.52 -1.53 0 0 1.52 -7.62 0 0 1.53 16.77 0 0 -1.53 3.04 0 0 -1.52 1.53 0 0 -1.52 -1.53 0 0 -1.53z" strokeWidth="1"></path>
-      <path d="M19.81 21.33h3.05v1.53h-3.05Z" strokeWidth="1"></path>
-      <path d="M19.81 13.71h1.52v3.05h-1.52Z" strokeWidth="1"></path>
-      <path d="m19.81 21.33 0 -4.57 -1.52 0 0 3.05 -3.05 0 0 1.52 4.57 0z" strokeWidth="1"></path>
-      <path d="M13.71 21.33h1.53v1.53h-1.53Z" strokeWidth="1"></path>
-      <path d="M13.71 12.19h1.53v1.52h-1.53Z" strokeWidth="1"></path>
-      <path d="M12.19 13.71h1.52v1.53h-1.52Z" strokeWidth="1"></path>
-      <path d="M1.52 10.67h12.19v1.52H1.52Z" strokeWidth="1"></path>
-      <path d="M9.14 22.86h1.53v1.52H9.14Z" strokeWidth="1"></path>
-      <path d="M7.62 15.24h4.57v1.52H7.62Z" strokeWidth="1"></path>
-      <path d="M7.62 24.38h1.52v1.52H7.62Z" strokeWidth="1"></path>
-      <path d="M4.57 21.33h4.57v1.53H4.57Z" strokeWidth="1"></path>
-      <path d="M4.57 16.76h3.05v1.52H4.57Z" strokeWidth="1"></path>
-      <path d="M3.05 25.9h4.57v1.53H3.05Z" strokeWidth="1"></path>
-      <path d="M3.05 18.28h1.52v3.05H3.05Z" strokeWidth="1"></path>
-      <path d="M1.52 27.43h1.53v1.52H1.52Z" strokeWidth="1"></path>
-      <path d="M0 28.95h1.52v1.52H0Z" strokeWidth="1"></path>
-    </g>
-  </svg>
-);
-
-const CodingIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-full h-full" fill="currentColor">
-    <g>
-      <path d="m1.52 9.14 28.96 0 0 19.81 1.52 0 0 -25.9 -1.52 0 0 4.57 -28.96 0 0 -4.57 -1.52 0 0 25.9 1.52 0 0 -19.81z" strokeWidth={1} />
-      <path d="M1.52 28.95h28.96v1.53H1.52Z" strokeWidth={1} />
-      <path d="M4.57 12.19v13.72h22.86V12.19Zm9.14 7.62h-1.52v1.52h-1.52v1.53H9.14v-1.53h1.53v-1.52h1.52v-1.52h-1.52v-1.53H9.14v-1.52h1.53v1.52h1.52v1.53h1.52Zm9.15 0h-6.1v-1.52h6.1Z" strokeWidth={1} />
-      <path d="M9.14 4.57h1.53V6.1H9.14Z" strokeWidth={1} />
-      <path d="M6.1 4.57h1.52V6.1H6.1Z" strokeWidth={1} />
-      <path d="M3.05 4.57h1.52V6.1H3.05Z" strokeWidth={1} />
-      <path d="M1.52 1.52h28.96v1.53H1.52Z" strokeWidth={1} />
-    </g>
-  </svg>
-);
+import constructionCone from "../src/assets/stickericon/construction cone.svg";
+import education from "../src/assets/stickericon/education.svg";
+import fireFolder from "../src/assets/stickericon/fire folder.svg";
+import pencil from "../src/assets/stickericon/pencil.svg";
+import project from "../src/assets/stickericon/project.svg";
+import technicalSkills from "../src/assets/stickericon/technical-skills.svg";
 
 const RETRO_ICONS = [
-  <CodingIcon key="coding" />,
-  <BrushIcon key="brush" />,
-  <CassetteIcon key="cassette" />,
-  <CdIcon key="cd" />,
-  <FolderIcon key="folder" />,
-  <InternetIcon key="internet" />,
+  <img src={constructionCone} alt="Construction Cone" className="w-full h-full object-contain" key="cone" />,
+  <img src={education} alt="Education" className="w-full h-full object-contain" key="education" />,
+  <img src={fireFolder} alt="Fire Folder" className="w-full h-full object-contain" key="folder" />,
+  <img src={pencil} alt="Pencil" className="w-full h-full object-contain" key="pencil" />,
+  <img src={project} alt="Project" className="w-full h-full object-contain" key="project" />,
+  <img src={technicalSkills} alt="Technical Skills" className="w-full h-full object-contain" key="skills" />,
 ];
+
 
 const RetroIcons = () => {
   return (
